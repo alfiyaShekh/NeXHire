d7646a2d9ac84ba03e20122a315aefaf366593af
Alfiya Shekh <alfiyashekh521@gmail.com>
2026-08-08 15:08:34 +0530

    Add Uvicorn server entrypoint to main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI(
     title="NexHire Backend",
@@ -8,3 +9,6 @@ app = FastAPI(
 @app.get("/")
 def home():
     return {"message": "NexHire Backend Running"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
